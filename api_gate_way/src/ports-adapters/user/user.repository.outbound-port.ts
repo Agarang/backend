ff21cddb364c0a7f00723cebe5c1683dbdba1e23
8471919 <7471919@naver.com>
2023-07-26 17:26:51 +0900

    feat: findUserForSignUp in UserRepository

--- api_gate_way/src/ports-adapters/user/user.repository.outbound-port.ts
+++ api_gate_way/src/ports-adapters/user/user.repository.outbound-port.ts
@@ -1,3 +1,4 @@
+import { UserEntity } from 'src/config/database/models/user.entity';
 import { CreateUserDto } from 'src/dtos/user/create-user.dto';
 
 export const USER_REPOSITORY_OUTBOUND_PORT =
@@ -5,4 +6,5 @@ export const USER_REPOSITORY_OUTBOUND_PORT =
 
 export interface UserRepositoryOutboundPort {
   insertUser(userInfo: CreateUserDto): Promise<CreateUserDto>;
+  findUserForSignUp(email: string): Promise<UserEntity | null>;
 }
--- api_gate_way/src/ports-adapters/user/user.repository.ts
+++ api_gate_way/src/ports-adapters/user/user.repository.ts
@@ -4,6 +4,7 @@ import { UserRepositoryOutboundPort } from './user.repository.outbound-port';
 import { CreateUserDto } from 'src/dtos/user/create-user.dto';
 import typia from 'typia';
 import { TypeToSelect } from 'src/utils/types/type-to-select.type';
+import { UserEntity } from 'src/config/database/models/user.entity';
 
 @Injectable()
 export class UserRepository implements UserRepositoryOutboundPort {
@@ -13,6 +14,16 @@ export class UserRepository implements UserRepositoryOutboundPort {
     const user = this.prisma.user.create({
       select: typia.random<TypeToSelect<CreateUserDto>>(),
       data: { ...userInfo, createdAt: new Date().toISOString() },
+    });
+
+    return user;
+  }
+
+  async findUserForSignUp(email: string): Promise<UserEntity | null> {
+    const user = this.prisma.user.findFirst({
+      where: {
+        email,
+      },
     });
 
     return user;
