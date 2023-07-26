da59b4e9021bdbd58a3f7d80a116c1e07e417fab
8471919 <7471919@naver.com>
2023-07-26 17:27:37 +0900

    feat: register in UserService

--- api_gate_way/src/domain/user/user.service.ts
+++ api_gate_way/src/domain/user/user.service.ts
@@ -1,4 +1,5 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/dtos/user/create-user.dto';
 import {
   USER_REPOSITORY_OUTBOUND_PORT,
   UserRepositoryOutboundPort,
@@ -10,4 +11,16 @@ export class UserService {
     @Inject(USER_REPOSITORY_OUTBOUND_PORT)
     private readonly userRepository: UserRepositoryOutboundPort,
   ) {}
+
+  async register(userInfo: CreateUserDto): Promise<CreateUserDto> {
+    const oldUser = this.userRepository.findUserForSignUp(userInfo.email);
+
+    if (oldUser !== null) {
+      throw new BadRequestException('중복된 이메일입니다.');
+    }
+
+    const user = this.userRepository.insertUser(userInfo);
+
+    return user;
+  }
 }
