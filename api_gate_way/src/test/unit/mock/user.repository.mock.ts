2494034fd4ab5be1003c66ecff83cf6b6fc755d0
8471919 <7471919@naver.com>
2023-07-26 17:47:01 +0900

    test: setting

---
+++ api_gate_way/src/test/unit/mock/user.repository.mock.ts
@@ -0,0 +1,31 @@
+import { UserEntity } from 'src/config/database/models/user.entity';
+import { CreateUserDto } from 'src/dtos/user/create-user.dto';
+import { UserRepositoryOutboundPort } from 'src/ports-adapters/user/user.repository.outbound-port';
+import { MockParamTypeForTest } from 'src/utils/types/mock-param-type-for-test.type';
+import { OmitProperties } from 'src/utils/types/omit.type';
+
+type MockUserRepositoryParamType = MockParamTypeForTest<MockUserRepository>;
+
+export class MockUserRepository implements UserRepositoryOutboundPort {
+  private readonly result: MockUserRepositoryParamType;
+
+  constructor(result: MockUserRepositoryParamType) {
+    this.result = result;
+  }
+
+  async insertUser(userInfo: CreateUserDto): Promise<CreateUserDto> {
+    const res = this.result.insertUser?.pop();
+    if (res === undefined) {
+      throw new Error('undefined');
+    }
+    return res;
+  }
+
+  async findUserForSignUp(email: string): Promise<UserEntity | null> {
+    const res = this.result.findUserForSignUp?.pop();
+    if (res === undefined) {
+      throw new Error('undefined');
+    }
+    return res;
+  }
+}
