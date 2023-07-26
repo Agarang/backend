ecbe1444f56912884fc501691faac97d0b5ba8e5
8471919 <7471919@naver.com>
2023-07-26 17:09:37 +0900

    fix: prisma schema

--- api_gate_way/src/config/database/prisma/prisma.service.ts
+++ api_gate_way/src/config/database/prisma/prisma.service.ts
@@ -1,10 +1,30 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: ['query'],
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
+
+    this.$extends({
+      query: {
+        $allModels: {
+          async create({ model, operation, args, query }) {
+            if (args.data.createdAt) {
+              args.data.createdAt = new Date().toISOString();
+            }
+
+            return query(args);
+          },
+        },
+      },
+    });
   }
 
   async enableShutdownHooks(app: INestApplication) {
