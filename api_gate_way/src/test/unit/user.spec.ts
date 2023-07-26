6904b5f4ad5e34d64848c2891cbdf17320f2f8f9
8471919 <7471919@naver.com>
2023-07-26 21:37:39 +0900

    test: Local Sign in in User Spec

--- api_gate_way/src/test/unit/user.spec.ts
+++ api_gate_way/src/test/unit/user.spec.ts
@@ -3,6 +3,10 @@ import { MockUserRepository } from './mock/user.repository.mock';
 import { UserController } from 'src/domain/user/user.controller';
 import typia from 'typia';
 import { CreateUserDto } from 'src/dtos/user/create-user.dto';
+import { UserEntity } from 'src/config/database/models/user.entity';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { AuthController } from 'src/auth/auth.controller';
 
 describe('User Spec', () => {
   describe('1. Register User', () => {
@@ -16,13 +20,42 @@ describe('User Spec', () => {
         }),
       );
 
-      console.log(userInfo);
-
       const userController = new UserController(userService);
 
       const res = await userController.register(userInfo);
 
       expect(res).toStrictEqual(userInfo);
+    });
+  });
+
+  describe('2. Sign In User', () => {
+    it('2-1. Local Sign In', async () => {
+      const user = typia.random<UserEntity>();
+
+      const authService = new AuthService(
+        new MockUserRepository({
+          findUserForSignUp: [user],
+        }),
+        new JwtService({
+          secret: 'secret',
+        }),
+      );
+
+      const authController = new AuthController(authService);
+
+      const res = await authController.localSignIn(
+        {
+          email: user.email,
+          id: user.id,
+          nickname: user.nickname,
+        },
+        {
+          email: user.email,
+          password: user.password,
+        },
+      );
+
+      expect(res.accessToken).toBeDefined();
     });
   });
 });
