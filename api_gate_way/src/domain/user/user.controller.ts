ea2342161cce1fd96dc48a4a104eeae43a3ee7c5
8471919 <7471919@naver.com>
2023-07-28 15:38:54 +0900

    feat: getOwnUserInfo in UserController

--- api_gate_way/src/domain/user/user.controller.ts
+++ api_gate_way/src/domain/user/user.controller.ts
@@ -1,7 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
-import { TypedBody, TypedRoute } from '@nestia/core';
+import { TypedBody, TypedParam, TypedRoute } from '@nestia/core';
 import { CreateUserDto } from 'src/dtos/user/create-user.dto';
+import { FindUserInfoOutboundPortOutputDto } from 'src/dtos/user/find-user-info.dto';
+import { JwtLocalGuard } from 'src/auth/guards/jwt-local.guard';
+import { User } from 'src/middlewares/decorators/user.decorator';
+import { LocalToken } from 'src/dtos/auth/local-token.dto';
 
 @Controller('/api/v1/user')
 export class UserController {
@@ -12,5 +16,17 @@ export class UserController {
     const user = await this.userService.register(userInfo);
 
     return user;
+  }
+
+  @UseGuards(JwtLocalGuard)
+  @TypedRoute.Get('info')
+  async getOwnUserInfo(
+    @User() user: LocalToken,
+  ): Promise<FindUserInfoOutboundPortOutputDto> {
+    const userId = user.id;
+
+    const userInfo = await this.userService.getUserInfo(userId);
+
+    return userInfo;
   }
 }
