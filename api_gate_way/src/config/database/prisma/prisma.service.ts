1ebe81db3d17f390ab67c921869fddb0f1a40815
8471919 <7471919@naver.com>
2023-08-07 12:01:56 +0900

    fix: add soft delete logic in find function of Prisma

--- api_gate_way/src/config/database/prisma/prisma.service.ts
+++ api_gate_way/src/config/database/prisma/prisma.service.ts
@@ -11,6 +11,30 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
 
   async onModuleInit() {
     await this.$connect();
+    await this.$extends({
+      query: {
+        user: {
+          async findMany({ model, operation, args, query }) {
+            args = {
+              where: {
+                deletedAt: null,
+              },
+              ...args,
+            };
+            return query(args);
+          },
+          async findFirst({ model, operation, args, query }) {
+            args = {
+              where: {
+                deletedAt: null,
+              },
+              ...args,
+            };
+            return query(args);
+          },
+        },
+      },
+    });
   }
 
   async enableShutdownHooks(app: INestApplication) {
