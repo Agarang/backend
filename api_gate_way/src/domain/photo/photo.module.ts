52e2f7ccd6566a91e526c35bd4e524c8e392a4a5
8471919 <7471919@naver.com>
2023-08-09 22:15:43 +0900

    feat: Azure Blob Storage

--- api_gate_way/src/domain/photo/photo.module.ts
+++ api_gate_way/src/domain/photo/photo.module.ts
@@ -4,9 +4,23 @@ import { PhotoService } from './photo.service';
 import { PrismaModule } from 'src/config/database/prisma/prisma.module';
 import { PhotoRepository } from 'src/ports-adapters/photo/photo.repository';
 import { PHOTO_REPOSITORY_OUTBOUND_PORT } from 'src/ports-adapters/photo/photo.repository.outbound-port';
+import { AzureStorageModule } from '@nestjs/azure-storage';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    AzureStorageModule.withConfigAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          sasKey: configService.get('AZURE_STORAGE_SAS_KEY')!,
+          accountName: configService.get('AZURE_STORAGE_ACCOUNT')!,
+          containerName: configService.get('AZURE_BLOB_STORAGE_CONTAINER')!,
+        };
+      },
+    }),
+    PrismaModule,
+  ],
   controllers: [PhotoController],
   providers: [
     {
