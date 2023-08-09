f1e3c03c599418c10ec1455a2e0b90cfe51245f9
8471919 <7471919@naver.com>
2023-08-09 21:55:24 +0900

    fix: move ConfigModule to AppModule from UserModule

--- api_gate_way/src/domain/user/user.module.ts
+++ api_gate_way/src/domain/user/user.module.ts
@@ -1,18 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'src/config/database/prisma/prisma.module';
 import { USER_REPOSITORY_OUTBOUND_PORT } from 'src/ports-adapters/user/user.repository.outbound-port';
 import { UserRepository } from 'src/ports-adapters/user/user.repository';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    PrismaModule,
-  ],
+  imports: [PrismaModule],
   controllers: [UserController],
   providers: [
     {
