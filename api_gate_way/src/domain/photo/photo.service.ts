bf958025748cffacab76426c07bc5057388479e1
8471919 <7471919@naver.com>
2023-08-10 19:39:34 +0900

    feat: uploadeProfilePhoto in PhotoService

--- api_gate_way/src/domain/photo/photo.service.ts
+++ api_gate_way/src/domain/photo/photo.service.ts
@@ -1,4 +1,10 @@
+import { UploadedFileMetadata } from '@nestjs/azure-storage';
 import { Inject, Injectable } from '@nestjs/common';
+import { UploadPhotoOutboundPortOutputDto } from 'src/dtos/photo/upload-profile-photo.dto';
+import {
+  AZURE_STORAGE_OUTBOUND_PORT,
+  AzureStorageOutboundPort,
+} from 'src/ports-adapters/azure/storage/azure.storage.outbound-port';
 import {
   PHOTO_REPOSITORY_OUTBOUND_PORT,
   PhotoRepositoryOutboundPort,
@@ -9,5 +15,30 @@ export class PhotoService {
   constructor(
     @Inject(PHOTO_REPOSITORY_OUTBOUND_PORT)
     private readonly photoRepository: PhotoRepositoryOutboundPort,
+
+    @Inject(AZURE_STORAGE_OUTBOUND_PORT)
+    private readonly azureStorage: AzureStorageOutboundPort,
   ) {}
+
+  async uploadProfilePhoto(
+    profile: UploadedFileMetadata,
+    userId: number,
+  ): Promise<UploadPhotoOutboundPortOutputDto> {
+    const { originalname, ...profileInfo } = profile;
+
+    const modifiedProfile = {
+      ...profileInfo,
+      originalname: `profile-${originalname}-${new Date().toISOString()}`,
+    };
+
+    const url = await this.azureStorage.uploadPhoto(modifiedProfile);
+
+    // 저장한 url을 DB 테이블에 저장
+    const storageUrl = await this.photoRepository.insertProfilePhotoUrl(
+      url.url,
+      userId,
+    );
+
+    return storageUrl;
+  }
 }
---
+++ api_gate_way/src/dtos/photo/upload-profile-photo.dto.ts
@@ -0,0 +1,3 @@
+export type UploadPhotoOutboundPortOutputDto = {
+  url: string;
+};
