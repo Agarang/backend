e07c4dcb054350bfffb6b1dd232e234112a31535
8471919 <7471919@naver.com>
2023-08-11 17:58:29 +0900

    chore: modify-file-name function

---
+++ api_gate_way/src/utils/functions/modify-file-name.function.ts
@@ -0,0 +1,26 @@
+import { UploadedFileMetadata } from '@nestjs/azure-storage';
+
+export function modifyFileName(
+  file: UploadedFileMetadata,
+  modifiedFileName: string,
+): UploadedFileMetadata {
+  const { originalname, ...fileInfo } = file;
+
+  const splitedFileNameArray = originalname.split('.');
+
+  if (splitedFileNameArray.length === 1) {
+    throw new Error('파일명을 파일이름.확장자 로 바꿔주세요.');
+  }
+
+  // 확장자명
+  const ext = splitedFileNameArray.slice(-1);
+
+  const title = splitedFileNameArray.slice(0, -1).join('.');
+
+  const modifiedFile = {
+    ...fileInfo,
+    originalname: `${modifiedFileName}-${title}-${new Date().toISOString()}.${ext}`,
+  };
+
+  return modifiedFile;
+}
