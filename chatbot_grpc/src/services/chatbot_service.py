404e77b3913551492656456c8a0cf665a5b39967
8471919 <7471919@naver.com>
2023-08-21 19:18:02 +0900

    feat: chatbot grpc server setting

---
+++ chatbot_grpc/src/services/chatbot_service.py
@@ -0,0 +1,22 @@
+from src.interface.proto.chatbot_pb2 import (
+    GetFetusResponseFromChatbotOutboundPortOutputDto,
+)
+from src.interface.proto import chatbot_pb2_grpc
+from datetime import datetime
+
+
+class ChatbotService(chatbot_pb2_grpc.ChatbotServiceServicer):
+    def __init__(self):
+        print("hi")
+
+    def getFetusResponseFromChatbot(self, request, context):
+        print("hi")
+
+        # input = request.input
+        # userId = request.userId
+
+        # createdAt = datetime.now().isoformat()
+
+        # return GetFetusResponseFromChatbotOutboundPortOutputDto(
+        #     response="hi", createdAt=createdAt
+        # )
