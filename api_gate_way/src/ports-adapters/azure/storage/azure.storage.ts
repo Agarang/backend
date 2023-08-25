889490d5a2a247cdbb2dab84f75d77cb70d9f15b
8471919 <7471919@naver.com>
2023-08-25 14:34:54 +0900

    fix: change azure storage package in AzureStorage

--- api_gate_way/src/ports-adapters/azure/storage/azure.storage.ts
+++ api_gate_way/src/ports-adapters/azure/storage/azure.storage.ts
@@ -1,24 +1,45 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, OnModuleInit } from '@nestjs/common';
 import { AzureStorageOutboundPort } from './azure.storage.outbound-port';
-import {
-  AzureStorageService,
-  UploadedFileMetadata,
-} from '@nestjs/azure-storage';
 import { UploadPhotoOutboundPortOutputDto } from 'src/dtos/photo/upload-profile-photo.dto';
+import { ConfigService } from '@nestjs/config';
+import { BlobServiceClient } from '@azure/storage-blob';
 
 @Injectable()
-export class AzureStorage implements AzureStorageOutboundPort {
-  constructor(private readonly azureStorage: AzureStorageService) {}
-  async uploadPhoto(
-    modifiedProfile: UploadedFileMetadata,
-  ): Promise<UploadPhotoOutboundPortOutputDto> {
-    const sasUrl = await this.azureStorage.upload(modifiedProfile);
+export class AzureStorage implements AzureStorageOutboundPort, OnModuleInit {
+  private blobServiceClient: BlobServiceClient;
 
-    if (!sasUrl) {
+  constructor(private readonly configService: ConfigService) {}
+
+  onModuleInit() {
+    const connectString = this.configService.get(
+      'AZURE_STORAGE_CONNECTION_STRING',
+    );
+
+    this.blobServiceClient =
+      BlobServiceClient.fromConnectionString(connectString);
+  }
+
+  async uploadPhoto(
+    modifiedProfile: Express.Multer.File,
+  ): Promise<UploadPhotoOutboundPortOutputDto> {
+    const fileName = modifiedProfile.originalname;
+
+    const containerClient = this.blobServiceClient.getContainerClient(
+      this.configService.get('AZURE_BLOB_STORAGE_CONTAINER')!,
+    );
+
+    const blockBlobClient = containerClient.getBlockBlobClient(fileName);
+
+    const url = blockBlobClient.url;
+
+    const uploadBlobResponse = await blockBlobClient.upload(
+      modifiedProfile.buffer,
+      modifiedProfile.size,
+    );
+
+    if (uploadBlobResponse.errorCode) {
       throw new ConflictException('이미지가 저장되지 않았습니다.');
     }
-
-    const url = sasUrl.split('?')[0];
 
     return { url };
   }
